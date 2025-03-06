f01437a50d57f75791778f486cc7288b4c51b1e0
Debora Serra <debora.r.serra@gmail.com>
2025-03-06 08:44:24 -0800

    feat: add form

---
+++ form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+      form {
+        border: 1px solid #999;
+        border-radius: 4px;
+        padding: 12px 8px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 12px;
+        max-width: 500px;
+        margin: 128px auto 0;
+      }
+
+      label {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        width: 100%;
+      }
+
+      label input {
+        padding: 4px;
+        font-size: 16px;
+        border: 1px solid #cdcdcd;
+        border-radius: 4px;
+      }
+
+      label input:focus {
+        outline: none;
+        border-color: #a0a0a0;
+      }
+
+      button {
+        padding: 8px 16px;
+        font-size: 16px;
+        background-color: #710000;
+        color: #fff;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <form action="process.php" method="get">
+      <label for="numbers">Input a list of numbers separated by comma: <input type="text" name="numbers" id="numbers" /></label>
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
