987e8e2d815282f9d54adec519e820d66d5c4ab4
Debora Serra <debora.r.serra@gmail.com>
2025-03-06 09:31:18 -0800

    feat: add python file

--- process.php
+++ process.php
@@ -1,11 +1,14 @@
 <?php
 
-  $list = $_GET('numbers');
-  $threshold = $_GET('threshold');
+  $list = $_GET['numbers'];
+  $threshold = $_GET['threshold'];
 
   $command = escapeshellcmd("python3 bitwise_operations.py $list $threshold");
   $output = shell_exec($command);
   $host = $_SERVER['HTTP_HOST'];
-  echo $host;
+  echo "<div style='text-align: center; border: 1px solid #999; border-radius: 4px; padding: 12px 8px; max-width: 500px; margin: 128px auto 0;'>";
+  echo "<h1>$host</h1>";
+  echo "<br>";
   echo $output;
+  echo "</div>";
 ?>
